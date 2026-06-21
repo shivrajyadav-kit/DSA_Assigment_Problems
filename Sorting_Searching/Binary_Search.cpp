2b0529dda154d8578024d018144b262b0f2fe33e
Your Name <you@example.com>
2026-06-21 16:16:18 +0530

    added the problem

--- Sorting_Searching/Binary_Search.cpp
+++ Sorting_Searching/Binary_Search.cpp
@@ -1,42 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-   int n;
-   cin>>n;
-   long long a[n];
-   for(int i =0;i<n;i++){
-      cin>>a[i];
-   }
-   int m;
-   cin>>m;
-   int l = 0;
-   int r = n-1;
-   bool flag = false;
-   while(l<=r){
-      int mid = (l+r)/2;
-      if(a[mid]==m){
-         flag = true;
-         break;
-      }
-      else if(a[mid]>m){
-         r = mid-1;
-      }
-      else{
-         l = mid +1;
-      }
-
-   }
-   if(flag){
-      cout<<"YES";
-      return 0;
-   }
-   else{
-      cout<<"NO";
-   }
-
+int main(){
+    int n;
+    cin>>n;
+    long long arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    long long x;
+    cin>>x;
+    bool flag = false;
+    for(int i=0;i<n;i++){
+        if(arr[i] == x){
+            flag = true;
+            break;
+        }
+        if(arr[i]>x){
+            break;
+        }
+    }
+    if(flag){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+        
 }
-   
-
-   
-
